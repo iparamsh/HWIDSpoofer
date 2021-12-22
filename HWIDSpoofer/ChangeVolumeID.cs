dad5f058095d2908e60a4dfb466f687dc2dc6f89
anarchysmo <73321844+anarchysmo@users.noreply.github.com>
2021-12-21 16:10:01 -0800

    fixed bug with multiple drives

--- HWIDSpoofer/ChangeVolumeID.cs
+++ HWIDSpoofer/ChangeVolumeID.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,28 +15,31 @@ namespace HWIDSpoofer
         {
             DriveInfo[] drives = DriveInfo.GetDrives();
 
-            Process process = new Process();
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.UseShellExecute = false;
-            process.Start();
-
-            process.StandardInput.WriteLine("cd C:/");
-            process.StandardInput.Flush();
-            process.StandardInput.WriteLine("start Volumeid.exe");
-            process.StandardInput.Flush();
-
-            for (int ctr = 0; ctr < drives.Length; ctr++) 
+            for (int ctr = 0; ctr < drives.Length; ctr++)
             {
-                process.StandardInput.WriteLine("volumeid " +  drives[ctr].Name.Substring(0, 2) + " " + randomString(4) + "-" + randomString(4));
-                process.StandardInput.Flush();
-            }
 
-            process.StandardInput.Close();
-            Console.WriteLine(process.StandardOutput.ReadToEnd());
-            process.WaitForExit();
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+
+                process.StandardInput.WriteLine("cd C:/");
+                process.StandardInput.Flush();
+                process.StandardInput.WriteLine("start Volumeid.exe");
+                process.StandardInput.Flush();
+
+
+                process.StandardInput.WriteLine("volumeid " + drives[ctr].Name.Substring(0, 2) + " " + randomString(4) + "-" + randomString(4));
+                process.StandardInput.Flush();
+
+
+                process.StandardInput.Close();
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+                process.WaitForExit();
+            }
         }
 
         private static Random random = new Random();
